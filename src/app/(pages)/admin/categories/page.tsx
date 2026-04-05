b03c8d0f0b41a3c2b083169282c7b3b9e3db04d3
NgHuuNhat <nhat200901@gmail.com>
2026-04-06 04:50:02 +0700

    update

--- src/app/(pages)/admin/categories/page.tsx
+++ src/app/(pages)/admin/categories/page.tsx
@@ -1,26 +1,11 @@
-import { CategoryType, columns } from "./columns"
-import { DataTable } from "./data-table"
+import React from 'react'
+import { DataTable } from './data-table'
+import data from "../data.json"
 
-async function getData(): Promise<CategoryType[]> {
-    // Fetch data from your API here.
-    return [
-        {
-            id: "728ed52f",
-            name: "test1",
-            slug: "test-1",
-            createdAt: "01/01/2001",
-            updatedAt: "02/02/2002",
-        },
-        // ...
-    ]
-}
-
-export default async function DemoPage() {
-    const data = await getData()
-
+export default function categoriesPage() {
     return (
-        <div className="container mx-auto py-10">
-            <DataTable columns={columns} data={data} />
+        <div className='categories-layout'>
+            <DataTable data={data} />
         </div>
     )
 }
