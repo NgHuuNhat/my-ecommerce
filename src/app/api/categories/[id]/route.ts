65c8d3d8035a9c96d21133b30aa20c7eeb3e5eb9
NgHuuNhat <nhat200901@gmail.com>
2026-04-08 11:33:02 +0700

    update-await-params

--- src/app/api/categories/[id]/route.ts
+++ src/app/api/categories/[id]/route.ts
@@ -3,52 +3,58 @@ import { NextRequest } from "next/server";
 
 // PATCH /api/categories/:id
 export async function PATCH(
-  req: NextRequest,
-  { params }: { params: { id: string } }
+    req: NextRequest,
+    //   { params }: { params: { id: string } }
+    context: { params: Promise<{ id: string }> }
 ) {
-  try {
-    const body = await req.json();
-    const { id } = params;
+    try {
+        const { id } = await context.params
+        const body = await req.json();
+        // const { id } = params;
 
-    const existing = await getCategoryById(id);
+        const existing = await getCategoryById(id);
 
-    if (!existing) {
-      return Response.json(
-        { success: false, message: "Category not found" },
-        { status: 404 }
-      );
+        if (!existing) {
+            return Response.json(
+                { success: false, message: "Category not found" },
+                { status: 404 }
+            );
+        }
+
+        await updateCategory(id, body);
+
+        return Response.json({
+            success: true,
+        });
+    } catch (error) {
+        return Response.json(
+            { success: false, message: "Failed to update category" },
+            { status: 500 }
+        );
     }
-
-    await updateCategory(id, body);
-
-    return Response.json({
-      success: true,
-    });
-  } catch (error) {
-    return Response.json(
-      { success: false, message: "Failed to update category" },
-      { status: 500 }
-    );
-  }
 }
 
 // DELETE /api/categories/:id (soft delete)
 export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { id: string } }
+    req: NextRequest,
+    //   { params }: { params: { id: string } }
+    context: { params: Promise<{ id: string }> }
+
 ) {
-  try {
-    const { id } = params;
+    try {
+        // const { id } = params;
+        const { id } = await context.params
 
-    await deleteCategory(id);
 
-    return Response.json({
-      success: true,
-    });
-  } catch (error) {
-    return Response.json(
-      { success: false, message: "Failed to delete category" },
-      { status: 500 }
-    );
-  }
+        await deleteCategory(id);
+
+        return Response.json({
+            success: true,
+        });
+    } catch (error) {
+        return Response.json(
+            { success: false, message: "Failed to delete category" },
+            { status: 500 }
+        );
+    }
 }
