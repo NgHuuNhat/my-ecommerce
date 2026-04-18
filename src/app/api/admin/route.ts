1cc043a99077efc73fa2fdbff95cc418a61bd2da
NgHuuNhat <nhat200901@gmail.com>
2026-04-18 15:28:08 +0700

    update-delete-route-admin

--- src/app/api/admin/route.ts
+++
@@ -1,8 +0,0 @@
-import { createAdmin } from "@/app/features/admin/model"
-import { NextResponse } from "next/server"
-
-export const POST = async (req: Request) => {
-    const data = await req.json()
-    const admin = await createAdmin(data)
-    return NextResponse.json(admin)
-}
